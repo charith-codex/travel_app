855d314efe6f5fd2c0d026850a5e40caa2483881
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-01 22:20:36 +0530

    add NavBar section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <h1 className="font-bold text-teal-500">hello</h1>;
+  return (
+    <>
+    <Hero/>
+    <Camp />
+    <Guide />
+    <Features />
+    <GetApp />
+    </>
+  )
 }
