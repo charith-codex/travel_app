9d1656c10cd69c77c4719f92660a32438c844365
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-03 23:58:32 +0530

    modify hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Button from './Button'
 
 const Hero = () => {
   return (
@@ -16,6 +17,37 @@ const Hero = () => {
         />
         <h1 className='bold-52 lg:bold-88'>Putuk Truno Camp Area</h1>
         <p className='regular-16 mt-6 text-gray-30 xl:max-w[520px]'>We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app</p>
+        <div className="my-11 flex flex-wrap gap-5">
+        <div className="flex items-center gap-2">
+          {Array(5).fill(1).map((_, index) => (
+            <Image
+              src="/star.svg"
+              key={index}
+              alt="star"
+              width={24}
+              height={24}
+            />
+          ))}
+      </div>
+      
+          <p className='bold-16 lg:bold-20 text-blue-70'>
+            198k
+            <span className='regular-16 lg:regular-20 ml-1'>Excellent Reviews</span>
+          </p>
+        </div>
+        <div className="flex flex-col w-full gap-3 sm:flex-row">
+          <Button
+            type='button' 
+            title='Download App' 
+            variant='btn_green'
+          />
+          <Button
+            type='button' 
+            title='How we work?' 
+            icon='/play.svg'
+            variant='btn_white_text'
+          />
+        </div>
       </div>
     </section>
   )
