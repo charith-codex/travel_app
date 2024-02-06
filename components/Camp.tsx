0e55524b31f81ae9a174f36a503eb8108c212da5
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-06 23:58:59 +0530

    modify camp section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,6 +1,7 @@
+import { PEOPLE_URL } from "@/constants";
 import Image from "next/image";
 
-interface CampProps{
+interface CampProps {
   backgroundImage: string;
   title: string;
   subtitle: string;
@@ -8,23 +9,37 @@ interface CampProps{
 
 }
 
-const CampSite = ({backgroundImage, title, subtitle, peopleJoined}:
+const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }:
   CampProps) => {
-  return(
+  return (
     <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
         <div className="flexCenter gap-4">
           <div className="rounded-full bg-green-50 p-4">
-            <Image 
-            src='/folded-map.svg' 
-            alt='map' 
-            width={28} 
-            height={28} />
+            <Image
+              src='/folded-map.svg'
+              alt='map'
+              width={28}
+              height={28} />
           </div>
           <div className="flex flex-col gap-1">
             <h4 className="bold-18 text-white">{title}</h4>
             <p className="regular-14 text-white">{subtitle}</p>
           </div>
+        </div>
+        <div className="flexCenter gap-6">
+          <span className="flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((url) => (
+              <Image
+                className="inline-block h-10 w-10 rounded-full"
+                src={url}
+                key={url}
+                alt="person"
+                width={52}
+                height={52} />
+            ))}
+          </span>
+          <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
         </div>
       </div>
     </div>
@@ -33,15 +48,20 @@ const CampSite = ({backgroundImage, title, subtitle, peopleJoined}:
 
 const Camp = () => {
   return (
-    <section className='border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
+    <section className=' 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite
           backgroundImage='bg-bg-img-1'
           title='Aguas Calientes'
-          subtitle='Peru'
-          peopleJoined='1.4k'
+          subtitle='Peru, South America'
+          peopleJoined='50+ Joined'
         />
-        <CampSite/>
+        <CampSite
+          backgroundImage='bg-bg-img-2'
+          title='Mount Everest'
+          subtitle='Nepal, Asia'
+          peopleJoined='20+ Joined'
+        />
       </div>
 
     </section>
