cf0c3a2fc1d05450ca56405a402982fd6152966b
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-07 21:19:25 +0530

    complete camp section
    start guide section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -63,7 +63,19 @@ const Camp = () => {
           peopleJoined='20+ Joined'
         />
       </div>
-
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white"><strong>Feeling Lost</strong> And Not Knowing The Way?</h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure</p>
+          <Image
+            src="/quote.svg"
+            alt="camp-2"
+            width={186}
+            height={219}
+            className="camp-quote"
+          />
+        </div>
+      </div>
     </section>
   )
 }
--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Guide = () => {
   return (
-    <div>Guide</div>
+    <section className='flexCenter flex-col'>
+      <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+        <Image
+          src='/camp.svg'
+          alt='camp'
+          width={50}
+          height={50}
+        />
+        <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>WE ARE HERE FOR YOU</p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2 className='bold-40 lg:bold-64 xl:max-w-[390px]'>Guide You to Easy Path</h2>
+          <p className='regular-16 text-gray-30 xl:max-w-[520px]'>Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
