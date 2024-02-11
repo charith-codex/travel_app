b33e86edf0875273badc67da67ee1bcdc47cf9a5
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-11 12:56:59 +0530

    complete feature section

--- components/Features.tsx
+++ components/Features.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <section className='border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
-      <div className="max-container padding-container relative w-full justify-end">
+    <section className='flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
+      <div className="max-container padding-container relative w-full flex justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
@@ -27,13 +27,12 @@ const Features = () => {
             />
             <h2 className='bold-40 lg:bold-64'>Our Features</h2>
           </div>
-          <ul>
+          <ul className='mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20'>
             {FEATURES.map((feature) => (
               <FeatureItem
                 key={feature.title}
                 title={feature.title}
                 icon={feature.icon}
-                variant={feature.variant}
                 description={feature.description}
               />
             ))}
@@ -46,14 +45,28 @@ const Features = () => {
 
 type FeatureItem = {
   title: String;
-
+  icon: String;
+  description: String;
 }
 
-const FeatureItem = ({ title, icon, variant,, description }: FeatureItem) => {
+const FeatureItem = ({ title, icon, description }: FeatureItem) => {
   return (
-    <div>
-      {title}
-    </div>
+    <li className='flex w-full flex-1 flex-col items-start'>
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+        <Image
+          src={icon}
+          alt='map'
+          width={28}
+          height={28}
+        />
+      </div>
+      <h2 className='bold-20 lg:bold-32 mt-5 capitalize'>
+        {title}
+      </h2>
+      <p className='regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none'>
+        {description}
+      </p>
+    </li>
   )
 }
 
