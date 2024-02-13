c2bc316ef4e2f36a7ec30f762083bd902f0df729
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-13 23:55:33 +0530

    1st part complete on footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image
+              src="hilink-logo.svg"
+              alt="logo"
+              width={74}
+              height={29}
+            />
+          </Link>
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            
+          </div>
+        </div>
+      </div>
+
+    </footer>
   );
 };
 
