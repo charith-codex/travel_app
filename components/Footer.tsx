6c4de901299830462a63838cb5a27ffef1e89ca1
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-16 23:54:24 +0530

    complete footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FOOTER_CONTACT_INFO, FOOTER_LINKS } from "@/constants";
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -33,24 +33,42 @@ const Footer = () => {
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
                 {FOOTER_CONTACT_INFO.links.map((link) => (
                   <Link
-                  href="/"
-                  key={link.label}
-                  className="flex gap-4 md:flex-col lg:flex-row"
+                    href="/"
+                    key={link.label}
+                    className="flex gap-4 md:flex-col lg:flex-row"
                   >
-                  <p className="whitespace-nowrap">
-                    {link.label}:
-                  </p>
-                  <p className="medium-14 whitespace-nowrap text-blue-70">
-                    {link.value}
-                  </p>
+                    <p className="whitespace-nowrap">
+                      {link.label}:
+                    </p>
+                    <p className="medium-14 whitespace-nowrap text-blue-70">
+                      {link.value}
+                    </p>
                   </Link>
                 ))}
               </FooterColumn>
             </div>
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={SOCIALS.title}>
+                <ul className="regular-14 flex gap-4 text-gray-30">
+                  {SOCIALS.links.map((link) => (
+                    <Link href="/" key={link}>
+                      <Image
+                        src={link}
+                        alt="logo"
+                        width={24}
+                        height={24} />
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
           </div>
         </div>
-      </div>
 
+        <div className="border bg-gray-20" />
+        <p className="regular-14 w-full text-center text-gray-30">2024 Hilink | All rights reserved</p>
+      </div>
     </footer>
   );
 };
